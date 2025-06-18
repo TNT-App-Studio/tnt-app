46849b2584bc7f10e69fd54ab067082849d8ef34
Huy Lê <huynl.dev@gmail.com>
2025-06-18 19:27:04 +0700

    add: privacy, contact and eula pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Contact } from './pages/contact/contact';
+import { Eula } from './pages/eula/eula';
+import { Privacy } from './pages/privacy/privacy';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'privacy', component: Privacy },
+  { path: 'eula', component: Eula },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: 'home' },
+];
